e3cebc782b3cd80c7929073510ef958f9c77d3db
Benji <f.nossin@rubika-edu.com>
2023-01-10 17:05:08 +0100

    UI Toolkit (jukebox)

---
+++ Projet Architecture/Assets/Scripts/Manager/JukeboxManager.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using System.Collections.Generic; 
+using UnityEngine.UIElements;
+using UnityEditor;
+using System.Linq;
+using System;
+
+public class JukeboxManager : MonoBehaviour
+{
+    [SerializeField] private UIDocument document;
+    [SerializeField] private Camera mainCam; // PLACEHOLDER for sound manager
+
+    [SerializeField] private VisualTreeAsset m_UXMLTree;
+
+    private const string m_ButtonPrefix = "Button_Music";
+
+    [Space, SerializeField] private AudioClip[] clips = new AudioClip[3];
+    private List<Button> musicButtons = new List<Button>();
+    private bool show;
+    private AudioSource mainSource;
+    VisualElement subRoot;    
+
+    private void Awake()
+    {
+        subRoot = document.rootVisualElement.Q<VisualElement>("JukeBox_Options");
+        musicButtons = subRoot.Children().ToList().ConvertAll(x => x.Q<Button>());
+        SetupButtonHandler();
+    }
+
+    private void Start()
+    {
+        mainSource = mainCam.GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        // remplacer par player trigger enter
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            show = !show; 
+            Display(); 
+        }
+    }
+
+    private void Display()
+    {
+        // TODO : tween show/hide animation + sfx
+        document.enabled = show;
+    }    
+
+    private void SetupButtonHandler()
+    {
+        musicButtons.ForEach(RegisterHandler);
+    }
+
+    private void RegisterHandler(Button button)
+    {
+        button.RegisterCallback<ClickEvent>(SetNewMusic);
+    }
+
+    private void SetNewMusic(ClickEvent evt)
+    {
+        // TODO : tween set new animation + sfx
+
+        Button button = evt.currentTarget as Button;
+        int buttonNumber = Int32.Parse(button.name.Substring(m_ButtonPrefix.Length));
+
+        mainSource.clip = clips[buttonNumber];
+        mainSource.Play();
+    }
+}
