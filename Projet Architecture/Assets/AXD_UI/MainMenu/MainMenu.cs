e2335202a8fbc1b13d7922fe87ff259ce28d2655
Alexandre Daspe <alexandre.daspe@gmail.com>
2023-01-24 15:57:07 +0100

    Main Menu

---
+++ Projet Architecture/Assets/AXD_UI/MainMenu/MainMenu.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]  UIDocument uiDoc;
+    private Button playButton;
+    private Button quitButton;
+    [SerializeField] private string playSceneName;
+
+    private void Awake()
+    {
+        playButton = uiDoc.rootVisualElement.Q<Button>("playButton");
+        quitButton = uiDoc.rootVisualElement.Q<Button>("quitButton");
+        playButton.clicked += Play;
+        quitButton.clicked += Quit;
+    }
+
+    private void Play()
+    {
+        Debug.Log("Play");
+        SceneManager.LoadScene(playSceneName);
+    }
+
+    private void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+} 
